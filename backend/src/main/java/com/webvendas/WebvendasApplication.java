86dbc3935c041312ddafc3191bc186356f07a3a0
dayannaclaudino <day_claudino@hotmail.com>
2022-08-12 14:42:08 -0300

    Projeto criado

---
+++ backend/src/main/java/com/webvendas/WebvendasApplication.java
@@ -0,0 +1,13 @@
+package com.webvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webvendas/WebvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
