c2a1b1d349b5844de5c1197e62f8214c0f541693
dayannaclaudino <day_claudino@hotmail.com>
2022-08-17 16:54:05 -0300

    Data select

--- backend/src/main/java/com/webvendas/repositorios/VendaRepository.java
+++ backend/src/main/java/com/webvendas/repositorios/VendaRepository.java
@@ -1,13 +1,21 @@
 package com.webvendas.repositorios;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.webvendas.entidades.Venda;
 
+@Repository
 public interface VendaRepository extends JpaRepository<Venda, Long>{
 
-	List<Venda> findAll();
+	
+	@Query("SELECT obj FROM Venda obj WHERE obj.date BETWEEN :minDate AND :maxDate ORDER BY obj.total DESC")
+	Page<Venda> findVendas(LocalDate minDate, LocalDate maxDate, Pageable pageable);
 	
 }
